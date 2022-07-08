b842e65512d8bb96bd67a482089d276577950031
Devrath <devrath@mplgaming.com>
2022-07-08 21:04:59 +0530

    code added

--- code/app/src/main/java/com/droid/code/MainActivity.kt
+++ code/app/src/main/java/com/droid/code/MainActivity.kt
@@ -3,7 +3,9 @@ package com.droid.code
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.droid.code.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding

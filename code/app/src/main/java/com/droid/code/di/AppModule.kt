cf008e6bd763e2537e398bd2f473762b2620feb4
Devrath <devrath@mplgaming.com>
2022-07-08 22:19:32 +0530

    v

--- code/app/src/main/java/com/droid/code/di/AppModule.kt
+++ code/app/src/main/java/com/droid/code/di/AppModule.kt
@@ -3,6 +3,7 @@ package com.droid.code.di
 import android.app.Application
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
+import com.droid.code.utils.Keys.SHARED_PREFS_FILE_NAME
 import com.droid.preference.data.DefaultPreferences
 import com.droid.preference.domain.Preferences
 import dagger.Module
@@ -20,7 +21,7 @@ object AppModule {
     fun provideSharedPreferences(
         app: Application
     ): SharedPreferences {
-        return app.getSharedPreferences("shared_pref", MODE_PRIVATE)
+        return app.getSharedPreferences(SHARED_PREFS_FILE_NAME, MODE_PRIVATE)
     }
 
     @Provides
--- code/app/src/main/java/com/droid/code/utils/Keys.kt
+++ code/app/src/main/java/com/droid/code/utils/Keys.kt
@@ -1,4 +1,5 @@
 package com.droid.code.utils
 
 object Keys {
+    const val SHARED_PREFS_FILE_NAME = "shared_pref"
 }

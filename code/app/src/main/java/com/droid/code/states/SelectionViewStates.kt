859de36506e75502a2a1f02ef4304bfa892e2adb
Devrath <devrath@mplgaming.com>
2022-07-09 16:42:56 +0530

    check

--- code/app/src/main/java/com/droid/code/states/SelectionViewStates.kt
+++
@@ -1,7 +0,0 @@
-package com.droid.code.states
-
-sealed class SelectionViewStates {
-    object InitialState : SelectionViewStates()
-    object MigrationSuccessful : SelectionViewStates()
-    data class ErrorState(val errorMessage: String) : SelectionViewStates()
-}
--- code/app/src/main/java/com/droid/code/view/maindemo/SelectionActivity.kt
+++ code/app/src/main/java/com/droid/code/view/maindemo/SelectionActivity.kt
@@ -2,11 +2,8 @@ package com.droid.code.view.maindemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import com.droid.code.R
 import com.droid.code.databinding.ActivitySelectionBinding
-import com.droid.code.states.SelectionViewStates
 import com.droid.code.utils.showToast
 import com.droid.code.utils.startActivity
 import com.droid.code.view.dataStore.DataStoreActivity
@@ -19,41 +16,18 @@ class SelectionActivity : AppCompatActivity()  {
 
     private lateinit var binding: ActivitySelectionBinding
 
-    private lateinit var viewModel: SelectionVm
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySelectionBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initViewModel();
         setOnClickListeners()
-        setupObserver()
     }
-
-    private fun initViewModel() { viewModel = ViewModelProvider(this)[SelectionVm::class.java] }
 
     private fun setOnClickListeners() {
         binding.apply {
             btnSharedPrefsId.setOnClickListener { startActivity<SharedPreferenceActivity>() }
-            btnMigrateId.setOnClickListener { viewModel.initiateMigration() }
             btnDataStoreId.setOnClickListener { startActivity<DataStoreActivity>() }
         }
     }
-
-    private fun startDataStoreScreen() {
-
-    }
-
-    /**
-     * Set the observer for the view states from view model
-     */
-    private fun setupObserver() {
-        lifecycleScope.launchWhenStarted { viewModel.viewState.collect { setViewState(it) } }
-    }
-
-    private fun setViewState(it: SelectionViewStates) {
-        when(it){
-            is SelectionViewStates.MigrationSuccessful -> showToast(resources.getString(R.string.m_success))
-        }
-    }
+    
 }
